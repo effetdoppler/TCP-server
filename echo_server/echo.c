c4da557ef28c11a8f09002165ddf5966b13b56cc
Charles Zhang <charles.zhang@epita.fr>
2022-02-07 16:28:26 +0100

    implement rewrite()

--- echo_server/echo.c
+++ echo_server/echo.c
@@ -6,7 +6,23 @@
 
 void rewrite(int fd, const void *buf, size_t count)
 {
-    // TODO (from a previous practical)
+    ssize_t res = write(fd, buf, count);
+    //If the return value of write() is smaller than its third argument, you must call write() a
+    //gain in order to have the rest of the data written. Repeat this until all the data has been sent
+    if (res != count)
+    {
+        char * buff  = buf;
+        while(res != count)
+        {
+            // If an error occurs, exit the program with an error message
+            if (res == -1)
+                err(EXIT_FAILURE, "write function has failed");
+            res = write(fd, buf, count);
+            buff = buff + res;
+            count = count - res;
+            res = write(fd, buff, count);
+        }
+    }
 }
 
 void echo(int fd_in, int fd_out)
