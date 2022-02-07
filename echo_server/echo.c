2b284835e3b6936d5f28b1fe480dd57d166ba990
Charles Zhang <charles.zhang@epita.fr>
2022-02-07 23:22:10 +0100

    implement echo()

--- echo_server/echo.c
+++ echo_server/echo.c
@@ -27,5 +27,18 @@ void rewrite(int fd, const void *buf, size_t count)
 
 void echo(int fd_in, int fd_out)
 {
-    // TODO
+    char buffer[BUFFER_SIZE];
+    while(1)
+    {
+        //Read some data from fd_in.
+        ssize_t r = read(fd_in, buffer, BUFFER_SIZE);
+        //When all the data has been read (r = 0), you must break the loop and return from the function
+        if (r == 0)
+            return;
+        //If an error occurs (r = -1), you must exit the program with an error message
+        if (r == -1)
+            err(EXIT_FAILURE, "read function has failed");
+        //When some data is in the buffer, you must send it to fd_out. To do so, use the rewrite() function
+        rewrite(fd_out, buffer, r);
+    }
 }
