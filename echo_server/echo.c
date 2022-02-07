a615150f662b660bfaed3f65b680f5fcbcf83f2f
Charles Zhang <charles.zhang@epita.fr>
2022-02-07 16:24:36 +0100

    skeleton

---
+++ echo_server/echo.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+
+#define BUFFER_SIZE 512
+
+void rewrite(int fd, const void *buf, size_t count)
+{
+    // TODO (from a previous practical)
+}
+
+void echo(int fd_in, int fd_out)
+{
+    // TODO
+}
---
+++ echo_server/single_server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <err.h>
+#include "echo.h"
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+        errx(EXIT_FAILURE, "Usage:\n"
+            "Arg 1 = Port number (e.g. 2048)");
+
+    // TODO
+}
